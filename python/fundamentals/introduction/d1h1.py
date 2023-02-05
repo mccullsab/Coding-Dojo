5492ea4ad44f7d754800d17ce2f31d2781067d09
Abigail McCulloch <abby.mccull@gmail.com>
2023-02-05 14:36:06 -0500

    python

--- python/fundamentals/introduction/d1h1.py
+++ python/fundamentals/introduction/d1h1.py
@@ -1,4 +1,18 @@
-count = 0
-while count <= 5:
-    print("looping - ", count)
-    count += 1
+# Challenge 1:
+#   Fill in the missing code for the full_name function.
+#   Be sure to add the 2 parameters (and name them appropriately)
+#   Return the full name to get the desired output!
+
+def full_name(first, last):
+    full = (f"{first} {last}")
+    return(full)
+    # your code here!
+
+name1 = full_name("Eddie", "Aikau")
+print(name1) # should print Eddie Aikau
+
+name2 = full_name("Abby", "M")
+print(name2)
+
+# Challenge 2: 
+#   Call the function again using your own name and print the results!
