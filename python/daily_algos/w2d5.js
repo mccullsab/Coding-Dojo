d9a55d6403c00d82edad950f404017d096e25ae0
Abigail McCulloch <abby.mccull@gmail.com>
2023-02-17 16:16:21 -0500

    ninj

---
+++ python/daily_algos/w2d5.js
@@ -0,0 +1,45 @@
+/* 
+  Given an array of objects / dictionaries to represent new inventory,
+  and an array of objects / dictionaries to represent current inventory,
+  update the quantities of the current inventory
+  
+  if the item doesn't exist in current inventory, add it to the inventory
+  return the current inventory after updating it.
+*/
+
+const newInv1 = [
+    { name: "Grain of Rice", quantity: 9000 },
+    { name: "Peanut Butter", quantity: 50 },
+    { name: "Royal Jelly", quantity: 20 },
+];
+const currInv1 = [
+    { name: "Peanut Butter", quantity: 20 },
+    { name: "Grain of Rice", quantity: 1 },
+];
+
+const expected1 = [
+    { name: "Peanut Butter", quantity: 70 },
+    { name: "Grain of Rice", quantity: 9001 },
+    { name: "Royal Jelly", quantity: 20 },
+];
+
+const newInv2 = [];
+const currInv2 = [{ name: "Peanut Butter", quantity: 20 }];
+const expected2 = [{ name: "Peanut Butter", quantity: 20 }];
+
+const newInv3 = [{ name: "Peanut Butter", quantity: 20 }];
+const currInv3 = [];
+const expected3 = [{ name: "Peanut Butter", quantity: 20 }];
+
+
+
+
+// 1. Driver 🚗
+// 2. Presenter 👩‍💻
+// 3. Navigator 🧭
+
+// take 5-8 mins to write the pseudocode here:
+
+
+
+// create the function and decide what params it needs and what it will return
