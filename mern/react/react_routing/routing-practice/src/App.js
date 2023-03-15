d600306de5e9fb7a9fd16f6f463adf4bef16531c
Abigail McCulloch <abby.mccull@gmail.com>
2023-03-15 12:05:03 -0500

    update mwb

---
+++ mern/react/react_routing/routing-practice/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>Home Component</h1>
+      <Link to={"/about"}>Go to About </Link>
+    </div>
+  );
+}
+
+const About = (props) => {
+  return (
+    <div>
+      <h1 style={{ color: "blue" }}>About Component</h1>
+      <Link to={"/"}>Go Home</Link>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Routing Example</h1>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App
