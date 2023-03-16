82ecb04809cfd9d2c8ee7e803087a73af090aa3f
Abigail McCulloch <abby.mccull@gmail.com>
2023-03-15 21:00:24 -0500

    error msg

--- mern/react/react_routing/luke-api-walker/src/components/Planets.jsx
+++ mern/react/react_routing/luke-api-walker/src/components/Planets.jsx
@@ -4,6 +4,8 @@ import {useParams } from 'react-router-dom';
 
 const Planets = () => {
     const[planet, setPlanet] = useState("")
+    const [error, setError] = useState(false)
+
     const{id} = useParams();
 
 
@@ -15,17 +17,26 @@ const Planets = () => {
             setPlanet(response.data);
         })
         .catch((error) => {
-            console.log(error);
+            console.log("These aren't the droids you're looking for", error);
+            setError(true);
         });
     }, [id]);
 
     return(
         <div>
+            {error ?
+                <>
+                    <p>These aren't the droids you're looking for</p>
+                    <img src="https://m.media-amazon.com/images/M/MV5BMTc5OTMxMjY0NV5BMl5BanBnXkFtZTcwOTE1NjI4NA@@._V1_FMjpg_UX1000_.jpg" alt="ob1kenobi" />
+                </>
+                :
+                <>
             <h1>{planet.name}</h1>
             <p>Climate: {planet.climate}</p>
             <p>Terrain: {planet.terrain}</p>
             <p>Surface Water: {planet.surface_water > 0 ? 'true' : 'false'}</p>
             <p >Population: {planet.population}</p>
+            </>}
         </div>
     )
 };
